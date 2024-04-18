a4ab1d755e7c02a6bef19ecfebc49f877f2c733b
Mateus santos fernandes <mateusf.2022@alunos.utfpr.edu.br>
2024-04-18 10:40:48 -0300

    substituição de componentes input e button

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
-import Link from 'next/link';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 
 const Login = () => {
 
@@ -10,28 +11,30 @@ const Login = () => {
         <h1 className="font-bold italic text-7xl">CarsOnCars</h1>
       </div>
       
-      <div className="flex justify-center p-7 space-x-3 items-start gap-16 mt-20">
+      <div className="flex pl-48 p-7 space-x-3 items-start gap-16 mt-20">
 
         <form className="flex flex-col">
-          <h2 className='text-2xl justify-center flex'>Login</h2>
+          <h2 className='text-2xl justify-center flex font-semibold'>Login</h2>
           <label htmlFor="email" className='mt-2 ml-1'>Email</label>
-          <input type="email" name="email" id="email" className='rounded-full p-1 pr-20 border-gray-500 border outline-none'/>
+          <Input type="email"  id="email" className='rounded-full p-1 pr-20 border-[#898888] border outline-none'/>
           <label htmlFor="password" className='mt-5 ml-1'>Senha</label>
-          <input type="password" name="password" id="password" className='rounded-full p-1 border-gray-500 border outline-none'/>
-          <button type="submit" className='mt-5 p-2 bg-[#E1E1E1] rounded-lg border-gray-400 border'>Entrar</button>
+          <Input type="password" id="password" className='rounded-full p-1 border-[#898888] border outline-none'/>
+          <Button type="submit" className='mt-5 p-2 bg-[#D9D9D9] rounded-lg border-[#898888]'>Entrar</Button>
         </form>
 
-        <div className="border border-l border-gray-600 h-96"></div>
+        <div className="border border-l border-black h-96"></div>
 
         <form className="flex flex-col">
-          <h2 className='text-2xl justify-center flex'>Registrar</h2>
-          <label htmlFor="email" className='mt-2 ml-1'>Email</label>
-          <input type="email" name="email" id="email" className='rounded-full p-1 pr-20 border-gray-500 border outline-none'/>
+          <h2 className='text-2xl justify-center flex font-semibold'>Registrar</h2>
+          <label htmlFor="usuario" className='mt-2 ml-1'>Nome de usuário</label>
+          <Input type="text"  id="usuario" className='rounded-full p-1 pr-20 border-[#898888] border outline-none'/>
+          <label htmlFor="email" className='mt-5 ml-1'>Email</label>
+          <Input type="email"  id="email" className='rounded-full p-1 pr-20 border-[#898888] border outline-none'/>
           <label htmlFor="password" className='mt-5 ml-1'>Senha</label>
-          <input type="password" name="password" id="password" className='rounded-full p-1 border-gray-500 border outline-none'/>
-          <label htmlFor="password" className='mt-5 ml-1'>Confirmar senha</label>
-          <input type="password" name="password" id="password" className='rounded-full p-1 border-gray-500 border outline-none'/>
-          <button type="submit" className='mt-5 p-2 bg-[#E1E1E1] rounded-lg border-gray-400 border'>Registrar</button>
+          <Input type="password" id="password" className='rounded-full p-1 border-[#898888] border outline-none'/>
+          <label htmlFor="passwordconfirm" className='mt-5 ml-1'>Confirmar senha</label>
+          <Input type="password" id="passwordconfirm" className='rounded-full p-1 border-[#898888] border outline-none'/>
+          <Button type="submit" className='mt-5 p-2 bg-[#D9D9D9] rounded-lg border-[#898888]'>Registrar</Button>
         </form>
       </div>
 
